ea2ab57144eae54035b61f29f2aa58dd0078af57
Chris Knadler <takeshi91k@gmail.com>
2013-12-25 16:10:00 -0800

    Add lower case option to camelize
    Also adds several more test groups to camelize.

--- lib/nidyx/core_ext/string.rb
+++ lib/nidyx/core_ext/string.rb
@@ -1,6 +1,12 @@
 class String
-  def camelize
-    s = self.sub(/^[a-z\d]*/) { $&.capitalize }
+  # Inspired by (read: "Stolen from") Rails ActiveSupport::Inflector
+  def camelize(uppercase_first_letter = true)
+    s = self
+    if uppercase_first_letter
+      s = s.sub(/^[a-z\d]*/) { $&.capitalize }
+    else
+      s = s.sub(/^([A-Z_]|\w)/) { $&.downcase }
+    end
     s.gsub(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }
   end
 end
--- test/nidyx/core_ext/test_string.rb
+++ test/nidyx/core_ext/test_string.rb
@@ -7,7 +7,34 @@ class TestCoreExt < Minitest::Test
     assert_equal("CamelCaseString", "camel_case_string".camelize)
     assert_equal("CamelCaseString", "Camel_Case_String".camelize)
     assert_equal("CamelCaseString", "camel_Case_STRING".camelize)
+    assert_equal("1IsTheLoneliestNumber", "1_is_the_loneliest_number".camelize)
+    assert_equal("CCString", "CCString".camelize)
+  end
+
+  def test_camelize_with_optional_param
+    assert_equal("CamelCaseString", "camelCaseString".camelize(true))
+    assert_equal("camelCaseString", "CamelCaseString".camelize(false))
+    assert_equal("camelCaseString", "camel_case_string".camelize(false))
+    assert_equal("camelCaseString", "camel_case_string".camelize(false))
+    assert_equal("camelCaseString", "Camel_Case_String".camelize(false))
+    assert_equal("camelCaseString", "camel_Case_STRING".camelize(false))
+    assert_equal("1IsTheLoneliestNumber", "1_is_the_loneliest_number".camelize(false))
+  end
+
+  def test_camelize_unchanged
     assert_equal("CamelCaseString", "CamelCaseString".camelize)
-    assert_equal("1IsTheLonliestNumber", "1_is_the_lonliest_number".camelize)
+    assert_equal("CamelCaseString", "CamelCaseString".camelize(true))
+    assert_equal("camelCaseString", "camelCaseString".camelize(false))
+  end
+
+  def test_camelize_with_plain_numbers
+    assert_equal("123121151", "123121151".camelize)
+    assert_equal("123121151", "123121151".camelize(false))
+  end
+
+  def test_camelize_leaves_original_in_tact
+    string = "camel_case_string"
+    assert_equal("CamelCaseString", string.camelize)
+    assert_equal("camel_case_string", string)
   end
 end
