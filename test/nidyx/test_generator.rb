58620fc3be6648ef31bc1db7ee9db8a5696ae802
Chris Knadler <takeshi91k@gmail.com>
2014-01-02 14:36:14 -0800

    Chained refs tests

--- test/nidyx/test_generator.rb
+++ test/nidyx/test_generator.rb
@@ -1,10 +1,28 @@
-require "nidyx"
 require "minitest/autorun"
+require "nidyx" # we use some core_ext stuff that requires this
 require "nidyx/generator"
 
 class TestGenerator < Minitest::Test
   def setup
     @gen = Nidyx::Generator.new("TST", {})
+  end
+
+  # Do basic validation of a class's `.h` and `.m` file
+  # Return the model for further validation once this is complete
+  def validate_model_files(models, name, header_imports)
+    model = models[name]
+
+    # header
+    assert_equal(name, model[:h].name)
+    assert_equal(name + ".h", model[:h].file_name)
+    assert_equal(header_imports, model[:h].imports)
+
+    # implementation
+    assert_equal(name, model[:m].name)
+    assert_equal(name + ".m", model[:m].file_name)
+    assert_equal([name], model[:m].imports)
+
+    model
   end
 
   def test_empty_schema
@@ -30,17 +48,14 @@ class TestGenerator < Minitest::Test
     models = @gen.spawn(schema)
 
     ###
-    # root model tests
+    # root model
     ###
-    model = models["TSTModel"]
+    model = validate_model_files(models, "TSTModel", [])
 
     # header
-    assert_equal("TSTModel", model[:h].name)
-    assert_equal("TSTModel.h", model[:h].file_name)
     assert_equal(nil, model[:h].author)
     assert_equal(nil, model[:h].company)
     assert_equal(nil, model[:h].project)
-    assert_equal([], model[:h].imports)
 
     # properties
     props = model[:h].properties
@@ -58,12 +73,9 @@ class TestGenerator < Minitest::Test
     assert_equal(nil, value.desc)
 
     # implementation
-    assert_equal("TSTModel", model[:m].name)
-    assert_equal("TSTModel.m", model[:m].file_name)
     assert_equal(nil, model[:m].author)
     assert_equal(nil, model[:m].company)
     assert_equal(nil, model[:m].project)
-    assert_equal(["TSTModel"], model[:m].imports)
   end
 
   def test_deeply_nested_properties
@@ -90,14 +102,9 @@ class TestGenerator < Minitest::Test
     models = @gen.spawn(schema)
 
     ###
-    # root model tests
+    # root model
     ###
-    model = models["TSTModel"]
-
-    # header
-    assert_equal("TSTModel", model[:h].name)
-    assert_equal("TSTModel.h", model[:h].file_name)
-    assert_equal(["TSTValueModel"], model[:h].imports)
+    model = validate_model_files(models, "TSTModel", ["TSTValueModel"])
 
     # properties
     props = model[:h].properties
@@ -111,20 +118,10 @@ class TestGenerator < Minitest::Test
     assert_equal("object", value.type)
     assert_equal("TSTValueModel", value.class_name)
 
-    # implementation
-    assert_equal("TSTModel", model[:m].name)
-    assert_equal("TSTModel.m", model[:m].file_name)
-    assert_equal(["TSTModel"], model[:m].imports)
-
     ###
-    # first nested model
+    # value model
     ###
-    model = models["TSTValueModel"]
-
-    # header
-    assert_equal("TSTValueModel", model[:h].name)
-    assert_equal("TSTValueModel.h", model[:h].file_name)
-    assert_equal(["TSTValueObjModel"], model[:h].imports)
+    model = validate_model_files(models, "TSTValueModel", ["TSTValueObjModel"])
 
     # properties
     props = model[:h].properties
@@ -138,20 +135,10 @@ class TestGenerator < Minitest::Test
     assert_equal("object", obj.type)
     assert_equal("TSTValueObjModel", obj.class_name)
 
-    # implementation
-    assert_equal("TSTValueModel", model[:m].name)
-    assert_equal("TSTValueModel.m", model[:m].file_name)
-    assert_equal(["TSTValueModel"], model[:m].imports)
-
     ###
-    # second nested model
+    # value obj model
     ###
-    model = models["TSTValueObjModel"]
-
-    # header
-    assert_equal("TSTValueObjModel", model[:h].name)
-    assert_equal("TSTValueObjModel.h", model[:h].file_name)
-    assert_equal([], model[:h].imports)
+    model = validate_model_files(models, "TSTValueObjModel", [])
 
     # properties
     props = model[:h].properties
@@ -163,20 +150,13 @@ class TestGenerator < Minitest::Test
     data = props["data"]
     assert_equal("data", data.name)
     assert_equal("string", data.type)
-
-    # implementation
-    assert_equal("TSTValueObjModel", model[:m].name)
-    assert_equal("TSTValueObjModel.m", model[:m].file_name)
-    assert_equal(["TSTValueObjModel"], model[:m].imports)
   end
 
   def test_definitions
     schema = {
       "type" => "object",
       "properties" => {
-        "key" => {
-          "type" => "string"
-        },
+        "key" => { "type" => "string" },
         "value" =>  { "$ref" => "#/definitions/obj" },
         "banner" => { "$ref" => "#/definitions/banner" }
       },
@@ -184,31 +164,20 @@ class TestGenerator < Minitest::Test
         "obj" => {
           "type" => "object",
           "properties" => {
-            "name" => {
-              "type" => "string"
-            },
-            "count" => {
-              "type" => "integer"
-            }
+            "name" => { "type" => "string" },
+            "count" => { "type" => "integer" }
           }
         },
-        "banner" => {
-          "type" => "string"
-        }
+        "banner" => { "type" => "string" }
       }
     }
 
     models = @gen.spawn(schema)
 
     ###
-    # root model tests
+    # root model
     ###
-    model = models["TSTModel"]
-
-    # header
-    assert_equal("TSTModel", model[:h].name)
-    assert_equal("TSTModel.h", model[:h].file_name)
-    assert_equal(["TSTObjModel"], model[:h].imports)
+    model = validate_model_files(models, "TSTModel", ["TSTObjModel"])
 
     # properties
     props = model[:h].properties
@@ -220,25 +189,16 @@ class TestGenerator < Minitest::Test
     value = props["value"]
     assert_equal("value", value.name)
     assert_equal("object", value.type)
+    assert_equal("TSTObjModel", value.class_name)
 
     banner = props["banner"]
     assert_equal("banner", banner.name)
     assert_equal("string", banner.type)
 
-    # implementation
-    assert_equal("TSTModel", model[:m].name)
-    assert_equal("TSTModel.m", model[:m].file_name)
-    assert_equal(["TSTModel"], model[:m].imports)
-
     ###
-    # obj model tests
+    # obj model
     ###
-    model = models["TSTObjModel"]
-
-    # header
-    assert_equal("TSTObjModel", model[:h].name)
-    assert_equal("TSTObjModel.h", model[:h].file_name)
-    assert_equal([], model[:h].imports)
+    model = validate_model_files(models, "TSTObjModel", [])
 
     # properties
     props = model[:h].properties
@@ -250,50 +210,84 @@ class TestGenerator < Minitest::Test
     count = props["count"]
     assert_equal("count", count.name)
     assert_equal("integer", count.type)
-
-    # implementation
-    assert_equal("TSTObjModel", model[:m].name)
-    assert_equal("TSTObjModel.m", model[:m].file_name)
-    assert_equal(["TSTObjModel"], model[:m].imports)
   end
 
   def test_chained_definitions
     schema = {
       "type" => "object",
       "properties" => {
-        "obj1" => {
-          "key" => {
-            "type" => "string"
-          },
-          "value" => { "$ref" => "#/definitions/obj2" }
-        }
+        "key" => { "type" => "string" },
+        "value" => { "$ref" => "#/definitions/obj2" }
       },
       "definitions" => {
         "obj2" => {
           "type" => "object",
           "properties" => {
-            "key" => {
-              "type" => "string",
-            },
+            "key" => { "type" => "string" },
             "value" => { "$ref" => "#/definitions/obj3" }
           }
         },
         "obj3" => {
           "type" => "object",
           "properties" => {
-            "key" => {
-              "type" => "string",
-            },
-            "value" => {
-              "type" => "string"
-            }
+            "key" => { "type" => "string" },
+            "value" => { "type" => "string" }
           }
         }
       }
     }
 
     models = @gen.spawn(schema)
-    assert(models != nil)
+
+    ###
+    # root model
+    ###
+    model = validate_model_files(models, "TSTModel", ["TSTObj2Model"])
+
+    # properties
+    props = model[:h].properties
+
+    key = props["key"]
+    assert_equal("key", key.name)
+    assert_equal("string", key.type)
+
+    value = props["value"]
+    assert_equal("value", value.name)
+    assert_equal("object", value.type)
+    assert_equal("TSTObj2Model", value.class_name)
+
+    ###
+    # obj2 model
+    ###
+    model = validate_model_files(models, "TSTObj2Model", ["TSTObj3Model"])
+
+    # properties
+    props = model[:h].properties
+
+    key = props["key"]
+    assert_equal("key", key.name)
+    assert_equal("string", key.type)
+
+    value = props["value"]
+    assert_equal("value", value.name)
+    assert_equal("object", value.type)
+    assert_equal("TSTObj3Model", value.class_name)
+
+    ###
+    # obj3 model
+    ###
+    model = validate_model_files(models, "TSTObj3Model", [])
+
+    # properties
+    props = model[:h].properties
+
+    key = props["key"]
+    assert_equal("key", key.name)
+    assert_equal("string", key.type)
+
+    value = props["value"]
+    assert_equal("value", value.name)
+    assert_equal("string", value.type)
   end
 end
 
