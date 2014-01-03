4cb11239063eec6d0bff8211634f53135180ce65
Chris Knadler <takeshi91k@gmail.com>
2014-01-03 00:22:23 -0800

    Basic output working

--- lib/nidyx/model_base.rb
+++ lib/nidyx/model_base.rb
@@ -1,5 +1,8 @@
+require "mustache"
+
 module Nidyx
-  class ModelBase
+  class ModelBase < Mustache
+    self.template_path = "templates"
     attr_accessor :name, :file_name, :author, :company, :project, :imports
   end
 end
