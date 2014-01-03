56851cf3cef0901b9013323bfb4166b9062f9441
Chris Knadler <takeshi91k@gmail.com>
2014-01-03 00:33:11 -0800

    Reorganize output

--- lib/nidyx/model_base.rb
+++ lib/nidyx/model_base.rb
@@ -2,7 +2,7 @@ require "mustache"
 
 module Nidyx
   class ModelBase < Mustache
-    self.template_path = "templates"
+    self.template_path = "lib"
     attr_accessor :name, :file_name, :author, :company, :project, :imports
   end
 end
