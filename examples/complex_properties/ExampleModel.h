a6908bb81b4c86585e1ace82fd97bf450304dffd
Chris Knadler <takeshi91k@gmail.com>
2014-01-07 00:37:44 -0800

    Add complex properties examples

---
+++ examples/complex_properties/ExampleModel.h
@@ -0,0 +1,10 @@
+@interface ExampleModel
+
+@property (strong, nonatomic) NSArray *array;
+@property (strong, nonatomic) NSNumber *similar_stuff;
+@property (strong, nonatomic) id *not_really_similar_stuff;
+@property (strong, nonatomic) NSNumber *positive;
+@property (strong, nonatomic) id *nothing;
+@property (strong, nonatomic) NSNumber *not_required;
+
+@end
