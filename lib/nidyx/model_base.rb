6e5fcfa1863a423510e4303ba580c913113fce9d
Chris Knadler <takeshi91k@gmail.com>
2014-01-06 23:52:58 -0800

    Add -n / --no-comments flag

--- lib/nidyx/model_base.rb
+++ lib/nidyx/model_base.rb
@@ -3,13 +3,14 @@ require "mustache"
 module Nidyx
   class ModelBase < Mustache
     self.template_path = File.join(File.dirname(__FILE__), "..")
-    attr_accessor :name, :file_name, :author, :owner, :project, :imports
+    attr_accessor :name, :file_name, :author, :owner, :project, :imports, :comments
 
     def initialize(name, options)
       @name = name
       @author = options[:author] || ENV['USER']
       @owner = options[:company] || @author
       @project = options[:project]
+      @comments = options[:comments]
     end
 
     def created_date
