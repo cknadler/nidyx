50973d68867fe63ea7027b9180163bed90d0bd30
Chris Knadler <takeshi91k@gmail.com>
2014-01-20 20:57:40 -0800

    Add Mantle support ^_^

--- lib/nidyx/objc/interface.rb
+++ lib/nidyx/objc/interface.rb
@@ -2,7 +2,7 @@ require "nidyx/objc/model_base"
 
 module Nidyx
   class ObjCInterface < ObjCModelBase
-    attr_accessor :properties, :json_model
+    attr_accessor :properties, :json_model, :mantle
 
     self.template_file = File.join(self.template_path, "interface.mustache")
 
@@ -10,16 +10,23 @@ module Nidyx
       super
       self.file_name = "#{name}.#{EXT}"
       add_json_model if options[:objc][:json_model]
+      add_mantle if options[:objc][:mantle]
     end
 
     private
 
     EXT = "h"
     JSON_MODEL_IMPORT = "JSONModel"
+    MANTLE_IMPORT = "Mantle/Mantle.h"
 
     def add_json_model
       self.json_model = true
       self.imports << JSON_MODEL_IMPORT
+    end
+
+    def add_mantle
+      self.mantle = true
+      self.imports << MANTLE_IMPORT
     end
   end
 end
