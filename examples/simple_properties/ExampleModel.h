85945a6dae581c83ca9cbbb4400fde49548e764e
Chris Knadler <kndlrc@amazon.com>
2015-03-26 16:31:32 -0700

    Don't escape documentation strings
    Fixes #22

--- examples/simple_properties/ExampleModel.h
+++ examples/simple_properties/ExampleModel.h
@@ -8,7 +8,9 @@
 
 @property (strong, nonatomic) NSArray *array;
 @property (assign, nonatomic) BOOL boolean;
+// this is a comment that will show up as documentation
 @property (assign, nonatomic) NSInteger integer;
+// this is another comment with some 'special characters'
 @property (assign, nonatomic) double number;
 @property (strong, nonatomic) NSString *string;
 @property (strong, nonatomic) ExampleObjectModel *object;
